e80420423b81ffba26ea9959c4952a5e9161b125
Nanai10a <62282295+Nanai10a@users.noreply.github.com>
2023-05-18 10:42:38 +0900

    docs: 設計概要 (#5)
    * docs: write `ARCHITECTURE.md`
    
    * feat(docs): add `schema.ts` and define `Piece`
    
    `zod` didn't installed yet. please install it at other feature branch.
    
    * feat(docs): define `Settings`, `Leaderboard` roughly
    
    * fix: 伝わりづらい表現の修正
    
    Co-authored-by: ライガー <44018535+raiga0310@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: ライガー <44018535+raiga0310@users.noreply.github.com>

---
+++ docs/schema.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+// radwimpiece, radwimps / piece なので言語ごとの断片情報は "piece" である
+export type Piece = z.TypeOf<typeof Piece>;
+export const Piece = z.object({
+  // 言語名
+  name: z.string().nonempty(),
+
+  // ソースファイルの拡張子
+  // 概ね有名か, 最も使われている順とする
+  exts: z.string().nonempty().array(),
+
+  // 言語についての簡単な概要
+  about: z.string().nonempty(),
+
+  // リリース日？
+  // 確認可能な最大の精度とする (つまり不定, 取り扱い注意)
+  born: z.date(),
+
+  // `approvers/RADWIMPS` にあるソースコード
+  radwimps: z.string().nonempty(),
+
+  // 情報源へのリンク
+  sources: z.string().url().array(),
+});
+
+// 設定を保持するオブジェクト
+export type Settings = {
+  /* not defined */
+};
+
+// リーダーボードを保持するオブジェクト
+export type Leaderboard = {
+  current: LbEntry[];
+
+  push: (e: LbEntry) => void;
+};
